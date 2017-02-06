89348f69c7a0a1057fa41d7f08eb8f363dde7583
Tyler Blake <tyler.blake.louisville@gmail.com>
2017-02-05 22:37:15 -0500

    added email to student views, updated ui links/buttons, started creating ui for mass email functionality

--- DeveloperUniversity/DeveloperUniversity/Models/ViewModels/StudentViewModel.cs
+++ DeveloperUniversity/DeveloperUniversity/Models/ViewModels/StudentViewModel.cs
@@ -10,6 +10,7 @@ namespace DeveloperUniversity.Models.ViewModels
         public string LastName { get; set; }
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        public string Email { get; set; }
         [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
     }
